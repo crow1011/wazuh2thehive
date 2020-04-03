e851c5fee60f6d93c370b9de4386852ff5b20a90
crow1011 <crowsmith1011>
2020-04-03 17:27:39 +0300

    add requirements.txt and main

--- w2thive.py
+++ w2thive.py
@@ -7,6 +7,15 @@ import time
 import os
 import logging
 
+#!test!
+sys.argv.append('msg.alert')
+sys.argv.append('foo')
+sys.argv.append('oof')
+sys.argv.append('bar')
+#!test!
+
+
+
 # ossec.conf configuration:
 #  <integration>
 #      <name>w2thive</name>
@@ -18,8 +27,6 @@ import logging
 debug_enabled = False
 pwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 json_alert = {}
-now = time.strftime("%a %b %d %H:%M:%S %Z %Y")
-print(pwd)
 
 # Set paths
 log_file = '{0}/logs/integrations.log'.format(pwd+'/w2thive/')
@@ -34,5 +41,31 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-logger.debug('debug mode') # if debug enabled
 
+
+def main(args):
+    alert_file_location = args[1]
+    thive = args[2]
+    w_alert = json.load(open(alert_file_location))
+    print(w_alert.keys())
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+    try:
+        # Read arguments
+        if len(sys.argv) >= 4:
+            logger.warning('too many arguments')
+        elif len(sys.argv)<3:
+            logger.error('not enough arguments')
+        logger.debug('debug mode') # if debug enabled         
+        # Main function
+        main(sys.argv)
+
+    except Exception:
+        logger.exception('EGOR')
