06d90dfc09fc2df1bd5c977222abe46d6fffb5d0
crow1011 <crowsmith1011>
2020-04-07 09:44:41 +0300

    add recursion

--- w2thive.py
+++ w2thive.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-
-
 import json
 import sys
 import time
 import os
 import logging
-
+import requests
+import uuid
+from thehive4py.api import TheHiveApi
+from thehive4py.models import Alert
 #!test!
 sys.argv.append('msg.alert')
 sys.argv.append('foo')
@@ -18,16 +19,14 @@ sys.argv.append('bar')
 
 # ossec.conf configuration:
 #  <integration>
-#      <name>w2thive</name>
-#      <hook_url>http://localhost:9000</hook_url>
-#      <alert_format>json</alert_format>
+#    <name>w2thive</name>
+#    <hook_url>http://localhost:9000</hook_url>
+#    <alert_format>json</alert_format>
 #  </integration>
 
 # Global vars
 debug_enabled = False
 pwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-json_alert = {}
-
 # Set paths
 log_file = '{0}/logs/integrations.log'.format(pwd+'/w2thive/')
 logger = logging.getLogger(__name__)
@@ -44,28 +43,45 @@ logger.addHandler(fh)
 
 
 def main(args):
+    logger.debug('#start main')
     alert_file_location = args[1]
+    logger.debug('#get alert file location')
     thive = args[2]
+    logger.debug('#get TheHive url')
     w_alert = json.load(open(alert_file_location))
-    print(w_alert.keys())
+    logger.debug('#open alert file')
+    alt = pr(w_alert,'',[])
 
 
+def pr(data,prefix, alt):
+    for key,value in data.items():
+        if hasattr(value,'keys'):
+            pr(value,prefix+'.'+str(key),alt=alt)
+        else:
+            alt.append((prefix+'.'+str(key)+': '+str(value)))
+    return alt
 
 
+def generate_msg(alert):
+    pass
+
+
+def send_msg(msg, url):
+    pass
 
 
 
 if __name__ == "__main__":
     
     try:
-        # Read arguments
-        if len(sys.argv) >= 4:
-            logger.warning('too many arguments')
-        elif len(sys.argv)<3:
-            logger.error('not enough arguments')
-        logger.debug('debug mode') # if debug enabled         
-        # Main function
-        main(sys.argv)
+       # Read arguments
+       if len(sys.argv) >= 4:
+         logger.warning('too many arguments')
+       elif len(sys.argv)<3:
+         logger.error('not enough arguments')
+       logger.debug('debug mode') # if debug enabled       
+       # Main function
+       main(sys.argv)
 
     except Exception:
-        logger.exception('EGOR')
+       logger.exception('EGOR')
